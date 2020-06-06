5b17374c18ce9ededfbb753fa843d817684411e2
Snehal Bedmutha <snehal29@terpmail.umd.edu>
2020-06-06 15:27:21 -0400

    Removal of redundant code

--- Chatbot_GoogleSearch.py
+++ Chatbot_GoogleSearch.py
@@ -13,21 +13,8 @@ def chatbot_query(query, index=0):
     fallback = 'Sorry, I cannot think of a reply for that.'
     result = ''
 
-    GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
-    GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
-
-    def greeting(sentence):
-        """If user's input is a greeting, return a greeting response"""
-        for word in sentence.split():
-            if word.lower() in GREETING_INPUTS:
-                return random.choice(GREETING_RESPONSES)
-
-    if query[3:-1] in GREETING_INPUTS:
-        print("here")
-        return random.choice(GREETING_RESPONSES)
     try:
         search_result_list = googleSearch(query)
-        print(search_result_list)
         page = requests.get(search_result_list[index])
         tree = html.fromstring(page.content)
         soup = BeautifulSoup(page.content, features="lxml")
